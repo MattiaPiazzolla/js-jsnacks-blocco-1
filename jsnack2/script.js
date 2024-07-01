779d109729ab2da5afa08b509ac6a6b310a27416
MattiaPiazzolla <mattiapiazzolla@gmail.com>
2024-07-01 16:00:26 +0200

    [UPD] added a cycle to ask 6 times a prompt imput

--- jsnack2/script.js
+++ jsnack2/script.js
@@ -1,2 +1,7 @@
 // CREO UN ARRAY VUOTO
 let oddNumbers = [];
+// DEFINISCO UN CICLO FOR PER CHIEDERE 6 VOLTE DI INSERIRE UN NUMERO 
+for (let i = 0; i < 6; i++){
+    let number = prompt('Inserisci un numero')
+    console.log(number)
+}
