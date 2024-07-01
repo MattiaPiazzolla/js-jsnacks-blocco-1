321f6b55fad560625036586ed01c1d169ec33afd
MattiaPiazzolla <mattiapiazzolla@gmail.com>
2024-07-01 16:21:39 +0200

    [UPD] added event listener for the nuberSubBtn

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -1,2 +1,6 @@
 // RECUPERO IL PULSANTE DI INVIO DEL NUMERO
 const numberSubBtn = document.getElementById('submitNumber');
+// DICO AL PULSANTE DI ATTENDERE L'EVENTO CLICK
+numberSubBtn.addEventListener('click' function(){
+    
+})
