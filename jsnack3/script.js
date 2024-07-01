f1adf037747d57b135d5857cdfef715c71e03c6e
MattiaPiazzolla <mattiapiazzolla@gmail.com>
2024-07-01 16:35:59 +0200

    REVIEW

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -4,5 +4,11 @@ const numberSubBtn = document.getElementById('submitNumber');
 numberSubBtn.addEventListener('click', function(){
     // RECUPER IL VALORE DEL CAMPO IMPUT
     let numberInput = document.getElementById('numberInput').value;
-   
+    // CONTROLLO CHE IL NUMERO SIA EFFETTIVAMENTE DI 4 CIFRE 
+    if (numberInput.length !== 4) {
+        document.getElementById('output').innerHTML=`<p class="text-center text-danger mt-5">INSERIRE UN NUMERO DI 4 CIFRE</p>`
+    } else {
+        let cifre = parseInt(numberInput.split(''));
+        console.log(cifre)
+    }
 })
