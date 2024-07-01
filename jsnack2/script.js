42a07e8844827b275de6963601b0c254582d8657
MattiaPiazzolla <mattiapiazzolla@gmail.com>
2024-07-01 16:08:45 +0200

    [UPD/FIX] fixed the number from string to number with parseInt added the condition that check if the number is odd and added the odd number to the array

--- jsnack2/script.js
+++ jsnack2/script.js
@@ -2,6 +2,10 @@
 let oddNumbers = [];
 // DEFINISCO UN CICLO FOR PER CHIEDERE 6 VOLTE DI INSERIRE UN NUMERO 
 for (let i = 0; i < 6; i++){
-    let number = prompt('Inserisci un numero')
-    console.log(number)
+    let number = parseInt(prompt('Inserisci un numero'))
+    // CONTROLLO I NUMERI DISPARI E GLI INSERISCO NELL'ARRAY CON PUSH
+    if (number %2 !== 0){
+        oddNumbers.push(number)
+    }
 }
+console.log(oddNumbers)
