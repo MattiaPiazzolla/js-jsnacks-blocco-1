62d8f22a57e4172fc3ac739df33daf5b29287bf7
MattiaPiazzolla <mattiapiazzolla@gmail.com>
2024-07-01 15:04:14 +0200

    [UPD] defined a variable for the check button in the dom

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -1,2 +1,4 @@
 // DEFINISCO UN'ARRAY CON LA LISTA DEGLI INVITATI
 let guestList = ['fabrizio mastrobattista', 'luigi micco', 'alessio crea', 'Jay Gatsby', 'Nick Carraway', 'Daisy Buchanan', 'Myrtle Wilson', 'mattia piazzolla']; 
+// RECUPERO IL PULSANTE PER AVVIARE IL CHECK
+const logInButton = document.getElementById('checkName');
