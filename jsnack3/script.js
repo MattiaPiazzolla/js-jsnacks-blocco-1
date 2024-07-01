c8a96d68a67ab8dba2cf19fa6607f17953fcd524
MattiaPiazzolla <mattiapiazzolla@gmail.com>
2024-07-01 16:34:52 +0200

    [UPD] defined the variable for the numberinput

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -1,6 +1,8 @@
 // RECUPERO IL PULSANTE DI INVIO DEL NUMERO
 const numberSubBtn = document.getElementById('submitNumber');
 // DICO AL PULSANTE DI ATTENDERE L'EVENTO CLICK
-numberSubBtn.addEventListener('click' function(){
-    
+numberSubBtn.addEventListener('click', function(){
+    // RECUPER IL VALORE DEL CAMPO IMPUT
+    let numberInput = document.getElementById('numberInput').value;
+   
 })
