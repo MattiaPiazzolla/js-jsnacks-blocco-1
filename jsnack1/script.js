9985592a2f02cb44321081b266e6965381b27bef
MattiaPiazzolla <mattiapiazzolla@gmail.com>
2024-07-01 15:46:08 +0200

    [UPD] added the logic to check if the input is === to one element of the array added also style and output messages for every senario

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -10,6 +10,17 @@ checkNameBtn.addEventListener('click', function(){
     let invited = false;
     // CICLO L'ARRAI PER CONFRONTARE I SUOI ELEMENTI CON L'INPUT
     for (let i = 0; i < guestList.length; i++){
-        
+        // CONDIZIONE IN CUI IL VALORE DI INPUT COMBACIA CON UN ELEMENT DELL'ARRAY
+        if (guestList[i].toLocaleLowerCase() === nameInput){
+            invited = true;
+        }
     }
+    // MESSAGGIO DI OUTPUT
+    if (invited === true){
+        console.log("INVITATO")
+        document.getElementById('inviteTicket').classList.remove('d-none')
+    } else {
+        console.log("NON INVITATO")
+        document.getElementById('notInvited').innerHTML=`<p class="d-block text-center text-danger mt-5 fs-1">Non sei stato invitato alla festa</p>`
+}
 })
