6827290040a1a9ca7469555cbeef4813500f0b29
MattiaPiazzolla <mattiapiazzolla@gmail.com>
2024-07-01 15:07:49 +0200

    [UPD] defined event listener for the checkbutton

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -1,4 +1,8 @@
 // DEFINISCO UN'ARRAY CON LA LISTA DEGLI INVITATI
 let guestList = ['fabrizio mastrobattista', 'luigi micco', 'alessio crea', 'Jay Gatsby', 'Nick Carraway', 'Daisy Buchanan', 'Myrtle Wilson', 'mattia piazzolla']; 
 // RECUPERO IL PULSANTE PER AVVIARE IL CHECK
-const logInButton = document.getElementById('checkName');
+const checkNameBtn = document.getElementById('checkName');
+// DETERMINO LE ISTRUZIONI PER UN EVENTO CLICK DEL PULSANTE
+checkNameBtn.addEventListener('click', function(){
+    
+})
